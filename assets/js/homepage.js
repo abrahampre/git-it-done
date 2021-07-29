64ba40f30b272fe85c8c71803c0dbd6bd2bea6f3
abraham perez reyes <abrahampre27@gmail.com>
2021-07-28 22:15:15 -0500

    6.1.7

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,19 @@
-var getUserRepos = function (){
-    fetch("https://api.github.com/users/octocat/repos");
+var user = "abrahampre";
+
+var getUserRepos = function (user){
+   //format the github api url
+   var user = "facebook";
+   var apiUrl = "https://api.github.com/users/"+ user +"/repos";
+
+   //make a request to the url
+   fetch(apiUrl).then(function(response){
+       response.json().then(function(data){
+           console.log(data);
+       });
+   });
 };
 
-getUserRepos();
+getUserRepos()
+
+// var response = fetch("https://api.github.com/users/octocat/repos");
+// console.log(response);
